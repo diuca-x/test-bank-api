4bdd28a186dd72325c940e8973e521e30654756b
Diuca <diucapuig@gmail.com>
2024-07-28 17:20:36 +0200

    feat(back) added tranference logic and edited the format of the statements list

--- backend/bank_account/serializers.py
+++ backend/bank_account/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from django.utils import timezone
 from .models import Transactions,Account
 
+from schwifty import IBAN
 
 class TransactionsSerializer(serializers.ModelSerializer):
+    date = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model=Transactions
         fields = [
@@ -13,6 +14,8 @@ class TransactionsSerializer(serializers.ModelSerializer):
             "amount",
             "balance_at_time",
         ]
+    def get_date(self,obj):
+        return obj.date.date()
 
 class MoneyMovementSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,7 +28,7 @@ class MoneyMovementSerializer(serializers.ModelSerializer):
         if hasattr(self, 'initial_data'):
             unknown_keys = set(self.initial_data.keys()) - set(self.fields.keys())
             if unknown_keys:
-                raise ValidationError({"Got unexpected fields: ": list(unknown_keys)})
+                raise serializers.ValidationError({"Got unexpected fields: ": list(unknown_keys)})
         return data
     
     def validate_amount(self, value):
@@ -37,7 +40,7 @@ class MoneyMovementSerializer(serializers.ModelSerializer):
         validated_data['date'] = timezone.now()
         if (self.context.get("operation") == "deposit"):
             validated_data['balance_at_time'] = Account.objects.all().first().current_balance + validated_data.get("amount")
-        elif (self.context.get("operation") == "withdraw"):
+        elif (self.context.get("operation") == "withdraw" or self.context.get("operation") == "transfer"):
             validated_data['balance_at_time'] = Account.objects.all().first().current_balance - validated_data.get("amount")
             validated_data["amount"] = -validated_data["amount"]
         else: 
@@ -48,7 +51,23 @@ class TransferSerializer(serializers.Serializer):
     amount = serializers.FloatField()
     account = serializers.CharField(max_length=34)
 
+    def validate(self, data):
+        if hasattr(self, 'initial_data'):
+            unknown_keys = set(self.initial_data.keys()) - set(self.fields.keys())
+            if unknown_keys:
+                raise serializers.ValidationError({"Got unexpected fields: ": list(unknown_keys)})
+        return data
+    
     def validate_amount(self, value):
         if value <= 0:
             raise serializers.ValidationError("Amount must be greater than zero.")
+        return value
+    
+    def validate_account(self,value):
+
+        try:
+            IBAN(value)
+        except Exception as error:
+            raise serializers.ValidationError((error))
+
         return value

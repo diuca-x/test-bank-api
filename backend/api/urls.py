b6bcfc6b595d1f75907fc268b481b72fc5917ccc
Diuca <diucapuig@gmail.com>
2024-07-28 15:37:54 +0200

    feat(back) added url for transfer

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,8 +5,8 @@ from bank_account import views as bank_view
 
 urlpatterns = [
     path("get-statement",bank_view.StatementListAPIView.as_view()),
-    path("deposit",bank_view.DepositMoneyAPIView.as_view()),
-    path("withdraw",bank_view.WithdrawMoneyAPIView.as_view()),
-    #path("transfer",bank_view.WithdrawMoneyAPIView.as_view())
+    path("deposit",bank_view.MoveMoneyAPIView.as_view()),
+    path("withdraw",bank_view.MoveMoneyAPIView.as_view()),
+    path("transfer",bank_view.TransferMoneyAPIView.as_view())
 
 ]

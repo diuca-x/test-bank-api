5a15341d9dd16507b80258785cca802b9918f90f
Diuca <diucapuig@gmail.com>
2024-07-28 14:21:52 +0200

    feat(back) created routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
 from . import views
+from bank_account import views as bank_view
 
 urlpatterns = [
-    path("",views.api_home)
+    path("get-statement",bank_view.StatementListAPIView.as_view()),
+    path("deposit",bank_view.DepositMoneyAPIView.as_view()),
+    path("withdraw",bank_view.WithdrawMoneyAPIView.as_view()),
+    #path("transfer",bank_view.WithdrawMoneyAPIView.as_view())
+
 ]

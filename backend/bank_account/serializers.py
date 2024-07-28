3eb01b495930d237288703c99badc0261daa0c93
Diuca <diucapuig@gmail.com>
2024-07-28 14:20:38 +0200

    feat(back) created serializers for bank_account models

---
+++ backend/bank_account/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from django.utils import timezone
+from .models import Transactions,Account
+
+
+class TransactionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Transactions
+        fields = [
+            "date",
+            "amount",
+            "balance_at_time",
+        ]
+
+
+class DepositSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Transactions
+        fields = [
+            "amount",
+        ]
+
+    def validate(self, data):
+        if hasattr(self, 'initial_data'):
+            unknown_keys = set(self.initial_data.keys()) - set(self.fields.keys())
+            if unknown_keys:
+                raise ValidationError({"Got unexpected fields: ": list(unknown_keys)})
+        return data
+    
+    def validate_amount(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Amount must be greater than zero.")
+        return value
+
+    def create(self, validated_data):
+        validated_data['date'] = timezone.now()
+        if (self.context.get("operation") == "deposit"):
+            validated_data['balance_at_time'] = Account.objects.all().first().current_balance + validated_data.get("amount")
+        elif (self.context.get("operation") == "withdraw"):
+            validated_data['balance_at_time'] = Account.objects.all().first().current_balance - validated_data.get("amount")
+            validated_data["amount"] = -validated_data["amount"]
+        else: 
+            raise serializers.ValidationError("Error in operation")
+        return super().create(validated_data)

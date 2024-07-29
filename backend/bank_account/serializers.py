24a3ee34dc21711f30c605e11e82f845f35a0cd5
Diuca <diucapuig@gmail.com>
2024-07-29 17:19:51 +0200

    feat(back) added pagination logic, you can make the call by page number, first, or last

--- backend/bank_account/serializers.py
+++ backend/bank_account/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from django.utils import timezone
 from .models import Transactions,Account,operation_type
@@ -25,6 +27,33 @@ class StatementRequestSerializer(serializers.Serializer):
     dates = serializers.ListField(child=serializers.DateField(input_formats=['%d-%m-%Y'],format='%d-%m-%Y'),min_length=2,max_length=2,required=False,allow_null=True)
     order = serializers.ChoiceField(choices=["asc","desc"],required=False,allow_null=True)
 
+# to make the pagination
+class CustomPagination(PageNumberPagination):
+    page_size = 10  
+
+    def paginate_queryset(self, queryset, request, view=None):
+        page = request.query_params.get("page")
+        if page == "first":
+            request.query_params._mutable = True
+            request.query_params['page'] = 1
+            request.query_params._mutable = True
+        elif page == "last":
+            paginator = self.django_paginator_class(queryset, self.page_size)
+            request.query_params._mutable = True
+            request.query_params['page'] = paginator.num_pages
+            request.query_params._mutable = True
+        return super().paginate_queryset(queryset, request, view)
+
+    def get_paginated_response(self, data):
+        return Response({
+            "page": self.page.number,
+            "total_pages":self.page.paginator.num_pages,
+            "links": {
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link()
+            },
+            "data": data
+        })
     
 # serializer for the Transaction model in case of withdraw or deposit, it takes the amount to be taken or deposited and creates the rest
 # of the fields to save a Transactions instance
--- backend/bank_account/views.py
+++ backend/bank_account/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError,NotFound
 
 from .models import Transactions,Account
-from .serializers import TransactionsSerializer,MoneyMovementSerializer,TransferSerializer,StatementRequestSerializer
+from .serializers import TransactionsSerializer,MoneyMovementSerializer,TransferSerializer,StatementRequestSerializer,CustomPagination
 from .scripts import filter_query
 import datetime
 
@@ -13,19 +13,27 @@ class StatementListAPIView(mixins.ListModelMixin,generics.GenericAPIView):
     request_serializer_class = StatementRequestSerializer
     response_serializer_class = TransactionsSerializer
 
+    pagination_class=CustomPagination
+
     queryset = Transactions.objects.all()
+
+    
 
     def post(self, request, *args, **kwargs):
         request_serializer = self.request_serializer_class(data=request.data)    
+
         if(request_serializer.is_valid()):
             filters = request_serializer.validated_data
             queryset = self.get_queryset()   
+
             filtered_query = filter_query(queryset,filters)
             if not filtered_query.exists():
                 return Response({'message': 'No transactions found.'}, status=status.HTTP_204_NO_CONTENT)
-            
-            serializer = self.response_serializer_class(filtered_query, many=True)
-            return Response(serializer.data)
+
+            page = self.paginate_queryset(filtered_query)
+            if page is not None:
+                response_serializer = self.response_serializer_class(page, many=True)
+                return self.get_paginated_response(response_serializer.data)
         else:
             raise ValidationError(code=400,detail=request_serializer.errors)         
         
